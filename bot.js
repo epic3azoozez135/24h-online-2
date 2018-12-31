c64cf6cf5d3bf3b0e4b9b1de9cd787e43f8669af
Azoooooooooz <44898336+epic3azoozez135@users.noreply.github.com>
2018-12-31 03:14:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,4 +58,22 @@ client.on('message', function(message) {
     }
 });
 
+
+
+client.on('message', msg => {
+
+    if (msg.content == 'Qjoin') {
+        if (msg.member.voiceChannel) {
+
+     if (msg.member.voiceChannel.joinable) {
+         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
+     }
+    }
+}
+})
+client.on('ready', () => {
+    client.channels.get("528443791817048074").join();
+    });
+
+
 client.login(process.env.BOT_TOKEN);
