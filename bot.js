07a8e0e9eafb113ebb19f60baf376981587c7f88
Azoooooooooz <44898336+epic3azoozez135@users.noreply.github.com>
2018-12-31 03:20:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const { Client, Util } = require('discord.js');
 const client = new Discord.Client();
 const prefix = "#";
  
-const devs = ["516583452699656192"]
+const devs = ["510829335519821825"]
  
 const adminprefix = "#";
 client.on('message', message => {
