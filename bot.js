302789a378cb904ffe87a9b2d59e6976df7309ce
Azoooooooooz <44898336+epic3azoozez135@users.noreply.github.com>
2018-12-31 03:45:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -45,20 +45,4 @@ client.on('message', message => {
  
   });
 
-
-client.on('message', msg => {
-
-    if (msg.content == '#join') {
-        if (msg.member.voiceChannel) {
-
-     if (msg.member.voiceChannel.joinable) {
-         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
-     }
-    }
-}
-})
-client.on('ready', () => {
-    client.channels.get("510829335519821825").join();
-    });
-
 client.login(process.env.BOT_TOKEN);
